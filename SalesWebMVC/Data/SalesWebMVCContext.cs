cf155e7f727806add83fb49512c2761ccd46994d
Camila Brito <camilagbrito48@gmail.com>
2023-08-30 15:19:16 +0100

    Other entities and second migration

--- SalesWebMVC/Data/SalesWebMVCContext.cs
+++ SalesWebMVC/Data/SalesWebMVCContext.cs
@@ -11,5 +11,9 @@ namespace SalesWebMVC.Data
         }
 
         public DbSet<Department> Department { get; set; } 
+
+        public DbSet<Seller> Seller { get; set; }
+
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
