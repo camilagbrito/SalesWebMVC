484b3db06d21b184641509637b736998f4825e86
Camila Brito <camilagbrito48@gmail.com>
2023-08-31 14:55:58 +0100

    Delete Seller

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -24,5 +24,18 @@ namespace SalesWebMVC.Services
             _context.SaveChanges();
         }
 
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var seller = _context.Seller.Find(id);
+
+            _context.Seller.Remove(seller);
+            _context.SaveChanges();
+        }
+
     }
 }
