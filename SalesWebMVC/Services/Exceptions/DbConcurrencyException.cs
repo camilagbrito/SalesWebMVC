399a95b050d4122ac2136da49b4a551d63c35d25
Camila Brito <camilagbrito48@gmail.com>
2023-09-02 10:34:49 +0100

    Update seller and custom service exception

---
+++ SalesWebMVC/Services/Exceptions/DbConcurrencyException.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.DotNet.Scaffolding.Shared.Messaging;
+
+namespace SalesWebMVC.Services.Exceptions
+{
+    public class DbConcurrencyException: ApplicationException
+    {
+        public DbConcurrencyException(string message): base(message) { }
+
+    }
+}
--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SalesWebMVC.Data;
 using SalesWebMVC.Models;
+using SalesWebMVC.Services.Exceptions;
 
 namespace SalesWebMVC.Services
 {
@@ -36,6 +37,25 @@ namespace SalesWebMVC.Services
 
             _context.Seller.Remove(seller);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller seller)
+        {
+            if (!_context.Seller.Any(x => x.Id == seller.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+
+            try
+            {
+                _context.Update(seller);
+                _context.SaveChanges();
+            }
+            catch(DbConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
+       
         }
 
     }
